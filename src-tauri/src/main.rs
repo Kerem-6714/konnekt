6e10da0476272a382ee7d5110a6e0da2722806ce
Kerem-6714 <keremdehab6714@gmail.com>
2026-05-18 19:24:29 +0300

    Update main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,22 +1,17 @@
-// Prevents additional console window on Windows in release
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use local_ip_address::local_ip;
-use tauri::Manager; // Event gönderebilmek için Manager'ı ekledik
+use tauri::Manager;
 
 fn main() {
     tauri::Builder::default()
         .setup(|app| {
-            // Uygulama başlar başlamaz local IP'yi alıyoruz
             let ip_address = match local_ip() {
                 Ok(ip) => ip.to_string(),
                 Err(_) => "127.0.0.1".to_string(),
             };
 
-            // Ana pencereyi buluyoruz
             let main_window = app.get_webview_window("main").unwrap();
-            
-            // IP adresini "ip-event" adıyla ön yüze fırlatıyoruz
             main_window.emit("ip-event", ip_address).unwrap();
 
             Ok(())
